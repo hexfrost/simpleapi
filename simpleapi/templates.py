304b2a1e4c5f992aa42925b1e3e0d07f69ff03c1
Kaziamov <kaziamov@outlook.com>
2024-03-22 06:32:43 +0400

    Update interfaces

--- simpleapi/templates.py
+++ simpleapi/templates.py
@@ -21,7 +21,11 @@ class GetInterface(BaseInterface):
         raise NotImplementedError
 
     def get(self, *args, **kwargs):
-        return self.router.get(prefix, f"{self.rn}")(self._get)(*args, **kwargs)
+        return self.router.get(
+            prefix=f"{self.rn}",
+        )(
+            self._get
+        )(*args, **kwargs)
 
 
 class PostInterface(BaseInterface):
@@ -29,7 +33,7 @@ class PostInterface(BaseInterface):
         raise NotImplementedError
 
     def post(self, *args, **kwargs):
-        return self.router.post(prefix, f"{self.rn}")(self._post)(*args, **kwargs)
+        return self.router.post(prefix=f"{self.rn}")(self._post)(*args, **kwargs)
 
 
 class PatchInterface(BaseInterface):
@@ -37,7 +41,7 @@ class PatchInterface(BaseInterface):
         raise NotImplementedError
 
     def patch(self, *args, **kwargs):
-        return self.router.patch(prefix, f"{self.rn}")(_patch)(*args, **kwargs)
+        return self.router.patch(prefix=f"{self.rn}")(self._patch)(*args, **kwargs)
 
 
 class PutInterface(BaseInterface):
@@ -45,7 +49,7 @@ class PutInterface(BaseInterface):
         raise NotImplementedError
 
     def put(self, *args, **kwargs):
-        return self.router.put(prefix, f"{self.rn}")(_put)(*args, **kwargs)
+        return self.router.put(prefix=f"{self.rn}")(self._put)(*args, **kwargs)
 
 
 class DeleteInterface(BaseInterface):
@@ -53,15 +57,15 @@ class DeleteInterface(BaseInterface):
         raise NotImplementedError
 
     def delete(self, *args, **kwargs):
-        return self.router.delete(prefix, f"{self.rn}")(_delete)(*args, **kwargs)
+        return self.router.delete(prefix=f"{self.rn}")(self._delete)(*args, **kwargs)
 
 
-class OptionInterface(BaseInterface):
-    def _option(self, *args, **kwargs):
+class OptionsInterface(BaseInterface):
+    def _options(self, *args, **kwargs):
         raise NotImplementedError
 
-    def option(self, *args, **kwargs):
-        return self.router.option(prefix, f"{self.rn}")(_option)(*args, **kwargs)
+    def options(self, *args, **kwargs):
+        return self.router.options(prefix=f"{self.rn}")(self._options)(*args, **kwargs)
 
 
 class HeadInterface(BaseInterface):
@@ -69,36 +73,41 @@ class HeadInterface(BaseInterface):
         raise NotImplementedError
 
     def head(self, *args, **kwargs):
-        return self.router.head(prefix, f"{self.rn}")(_head)(*args, **kwargs)
+        return self.router.head(prefix=f"{self.rn}")(self._head)(*args, **kwargs)
 
 
 class TraceInterfaces(BaseInterface):
     def _trace(self, *args, **kwargs):
         raise NotImplementedError
 
-    def trace(self):
-        return self.router.get(prefix, f"{self.rn}")(_get)(*args, **kwargs)
+    def trace(self, *args, **kwargs):
+        return self.router.trace(prefix=f"{self.rn}")(self._trace)(*args, **kwargs)
 
 
 class ConnectInterface(BaseInterface):
     def _connect(self, *args, **kwargs):
         raise NotImplementedError
 
-    def connect(self):
-        return self.router.get(prefix, f"{self.rn}")(_get)(*args, **kwargs)
+    def connect(self, *args, **kwargs):
+        # return self.router.connect(prefix=f"{self.rn}")(self._connect)(*args, **kwargs)
+        pass
 
 
 class AbstractEndpoint(
-    GetInterface, PostInterface, PutInterface, PatchInterface, DeleteInterface, HeadInterface, OptionInterface, ):
+    GetInterface,
+    PostInterface,
+    PutInterface,
+    PatchInterface,
+    DeleteInterface,
+    HeadInterface,
+    OptionsInterface,
+):
     pass
 
 
-x
-return self.router.get(prefix, f"{self.rn}")(_get)(*args, **kwargs)
-
-
-class BaseWebhook(PostInterface, HeadInterface, OptionInterface):
+class BaseWebhook(PostInterface, HeadInterface, OptionsInterface):
     pass
 
 
 class BaseEndpoint(AbstractEndpoint):
+    pass
